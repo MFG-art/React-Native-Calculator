c458ab98264bf35340843da3c37f52ab4f1c904d
Matthew Davis <marrthistyping64@gmail.com>
2023-06-22 19:29:26 -0500

    made calculator design more cool

--- App.js
+++ App.js
@@ -7,59 +7,14 @@ import {onPressButton} from "./ButtonActions.js";
 
 export default function App() {
 const [text, setText] = useState('');
-//const arr = {"1",2,3,+,4,5,6,-7,8,9,0};
   return (
     <View style={styles.column}>
         <View style={styles.screenContainer}>
             <Text style={styles.screen}>{text}</Text>
         </View>
-          <View style={styles.row}>
-               <TouchableOpacity style={styles.numberKeys} onPress={()=>setText(text+"1")}>
-                 <Text>1</Text>
-               </TouchableOpacity>
-               <TouchableOpacity style={styles.numberKeys} onPress={()=>setText(text+"2")}>
-                 <Text>2</Text>
-               </TouchableOpacity>
-               <TouchableOpacity style={styles.numberKeys} onPress={()=>setText(text+"3")}>
-                 <Text>3</Text>
-               </TouchableOpacity>
-              <TouchableOpacity style={styles.numberKeys} onPress={()=>setText(text+"+")}>
-                <Text>+</Text>
-              </TouchableOpacity>
-          </View>
-          <View style={styles.row} >
-            <View style={styles.row}>
-              <TouchableOpacity style={styles.numberKeys} onPress={()=>setText(text+"4")}><Text>4</Text></TouchableOpacity>
-            </View>
-            <View style={styles.row}>
-              <TouchableOpacity style={styles.numberKeys} onPress={()=>setText(text+"5")}><Text style={styles.text}>5</Text></TouchableOpacity>
-            </View>
-            <View style={styles.row}>
-              <TouchableOpacity style={styles.numberKeys} onPress={()=>setText(text+"6")}><Text>6</Text></TouchableOpacity>
-            </View>
-            <TouchableOpacity style={styles.numberKeys} onPress={()=>setText(text+"-")}>
-              <Text>-</Text>
-          <View style={styles.row}>
-          <TouchableOpacity style={styles.numberKeys} onPress={()=>setText(text+"4")}>
-            <Text>4</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.numberKeys} onPress={()=>setText(text+"5")}>
-            <Text>5</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.numberKeys} onPress={()=>setText(text+"6")}>
-            <Text>6</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.numberKeys} onPress={()=>setText(text+"-")}>
-            <Text>-</Text>
-          </TouchableOpacity>
-          </View>
-           <View style={styles.row} >
-             <TouchableOpacity style={styles.numberKeys} onPress={()=>setText(text+"7")}>
-               <Text>7</Text>
         <View style={styles.row} >
              <TouchableOpacity style={styles.CButton} onPress={()=>setText('')}>
                <Text style={styles.CText}>C</Text>
-
              </TouchableOpacity>
              <TouchableOpacity style={styles.numberKeys} onPress={()=>setText(text+"8")}>
                <Text style={styles.text}>()</Text>
@@ -68,8 +23,6 @@ const [text, setText] = useState('');
                <Text style={styles.text}>%</Text>
              </TouchableOpacity>
             <TouchableOpacity style={styles.numberKeys} onPress={()=>setText(text+"*")}>
-              <Text>*</Text>
-
               <Text style={styles.text}>/</Text>
             </TouchableOpacity>
          </View>
@@ -135,8 +88,6 @@ const [text, setText] = useState('');
 }
 
 const styles = StyleSheet.create({
-
-
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -160,7 +111,6 @@ const styles = StyleSheet.create({
 
   },
   numberKeys:{
-     backgroundColor:'gray',
      backgroundColor:'darkgray',
      alignItems: 'center',
      padding: 10,
@@ -172,20 +122,11 @@ const styles = StyleSheet.create({
     column: {
       flex: 1,
       flexDirection: 'column',
-      margin: 2
-
     },
     row: {
       flex: 1,
       flexDirection: 'row',
-
-      margin: 2,
-
     },
-
-    text: {
-        alignItems: 'center',
-        justifyContent: 'center',
     text: {
       color:'#262626',
       fontSize: 20,
